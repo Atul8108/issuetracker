fbddb1d795239c7393dcc35cd90dc4b17330af20
Atul Shaw <shawatul4@gmail.com>
2023-05-01 17:43:59 +0530

    mongo atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://0.0.0.0:27017/db';
+const url = 'mongodb+srv://shawatul4:VJd1UKI70m8MHR5U@issurtracker.72saofu.mongodb.net/?retryWrites=true&w=majority';
 // mongoose.connect('url') 
 mongoose.connect(url);
-const db=mongoose.connection;
+const db = mongoose.connection;
 
 //If any Error then Getting this Line
-db.on('error',console.error.bind(console,"Error connecting to MongoDB"));   
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
 
-db.once('open',()=>{
+db.once('open', () => {
     console.log("Connected to Database :: MongoDB ")
 });
 
-module.exports=db;  //Exports db
+module.exports = db;  //Exports db
